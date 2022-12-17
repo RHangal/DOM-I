19f432a78fdebdff6413d16325ba893ff644f868
Rohan Hangal <rohanhangal@gmail.com>
2022-12-17 00:25:53 -0800

    finished the contacts sections

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ const nav = {
 
 const navKeys = Object.keys(nav)
 const navArr = []
-const navList = navKeys.forEach(key => {
+navKeys.forEach(key => {
   console.log(nav[key])
   return navArr.push(nav[key])
 })
@@ -82,5 +82,63 @@ const ctaImg = document.querySelector("#cta-img")
 ctaH1.textContent = cta.h1
 ctaButton.textContent = cta.button
 ctaImg.setAttribute("src", "http://localhost:9000/img/cta.png")
+
+
+const mainContent = {
+  "features-h4": "Features",
+  "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "about-h4": "About",
+  "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "services-h4": "Services",
+  "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "product-h4": "Product",
+  "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "vision-h4": "Vision",
+  "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+}
+
+const topContent1 = document.querySelectorAll(".top-content .text-content h4")[0]
+const topContent2 = document.querySelectorAll(".top-content .text-content h4")[1]
+topContent1.textContent = mainContent["features-h4"]
+topContent2.textContent = mainContent["about-h4"]
+
+const topContent1P = document.querySelectorAll(".top-content .text-content p")[0]
+const topContent2P = document.querySelectorAll(".top-content .text-content p")[1]
+topContent1P.textContent = mainContent['features-content']
+topContent2P.textContent = mainContent['about-content']
+
+const middleImg = document.querySelector("#middle-img")
+middleImg.setAttribute('src', "http://localhost:9000/img/accent.png")
+
+const bottomContent1 = document.querySelectorAll(".bottom-content .text-content h4")[0]
+const bottomContent2 = document.querySelectorAll(".bottom-content .text-content h4")[1]
+const bottomContent3 = document.querySelectorAll(".bottom-content .text-content h4")[2]
+bottomContent1.textContent = mainContent["services-h4"]
+bottomContent2.textContent = mainContent["product-h4"]
+bottomContent3.textContent = mainContent["vision-h4"]
+
+const bottomContent1P = document.querySelectorAll(".bottom-content .text-content p")[0]
+const bottomContent2P = document.querySelectorAll(".bottom-content .text-content p")[1]
+const bottomContent3P = document.querySelectorAll(".bottom-content .text-content p")[2]
+bottomContent1P.textContent = mainContent["services-content"]
+bottomContent2P.textContent = mainContent["product-content"]
+bottomContent3P.textContent = mainContent["vision-content"]
+
+const contact = {
+  "contact-h4": "Contact",
+  "address": "123 Way 456 Street Somewhere, USA",
+  "phone": "1 (888) 888-8888",
+  "email": "sales@greatidea.io",
+}
+const contactKeys = Object.keys(contact)
+const contactArr = []
+contactKeys.forEach(key => contactArr.push(contact[key]))
+console.log(contactArr)
+
+
+const contactSection = document.querySelectorAll(".contact *")
+console.log(contactSection)
+contactSection.forEach((links,index) => links.textContent = contactArr[index])
+
 
 console.log('project wired!')
